a8092e6dfe2bdb8158e7ed5d5518013aed8daa78
Sougata Nayak <sougatanayak007@gmail.com>
2021-06-20 14:26:25 +0530

    Initial commit

---
+++ lib/Screens/queue_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QueueScreen extends StatelessWidget {
+  const QueueScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Text('My app'),
+    );
+  }
+}
